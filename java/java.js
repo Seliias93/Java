102b80a1255d53b1b78426425cb7e034639112e5
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-09-27 23:58:28 -0300

    .

---
+++ java/java.js
@@ -0,0 +1,11 @@
+do {
+	package
+} while (condition); com.mycompany.primeiroprograma;
+do {
+	public
+} while (condition); class PrimeiroPrograma {
+
+    public static void main([] args) {
+        System.out.println("Hello World!");
+    }
+}
