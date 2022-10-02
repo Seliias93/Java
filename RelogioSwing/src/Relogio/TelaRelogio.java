fffdd11db84d55b795c4aba08037486ed1b35ca1
Elias De Souza <106115252+Seliias93@users.noreply.github.com>
2022-10-02 13:58:10 -0300

    .

--- RelogioSwing/src/Relogio/TelaRelogio.java
+++ RelogioSwing/src/Relogio/TelaRelogio.java
@@ -3,18 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cursoemvideo.olamundo;
+package Relogio;
+
+import java.util.Date;
 
 /**
  *
- * @author PC
+ * @author Joao
  */
-public class OlaMundoJava extends javax.swing.JFrame {
+public class TelaRelogio extends javax.swing.JFrame {
 
     /**
-     * Creates new form OlaMundoJava
+     * Creates new form TelaRelogio
      */
-    public OlaMundoJava() {
+    public TelaRelogio() {
         initComponents();
     }
 
@@ -27,53 +29,62 @@ public class OlaMundoJava extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblMesangem = new javax.swing.JLabel();
-        btnClick = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        lblHo = new javax.swing.JLabel();
+        btnH = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblMesangem.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        lblMesangem.setForeground(new java.awt.Color(255, 0, 51));
-        lblMesangem.setText("Aqui vai aparecer a Mensagem");
+        jLabel1.setText("Data e Hora do Sistema ");
 
-        btnClick.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        btnClick.setText("Clique em mim!");
-        btnClick.addActionListener(new java.awt.event.ActionListener() {
+        lblHo.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblHo.setText("Click no Botão");
+
+        btnH.setText("Aperte Aqui");
+        btnH.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnClickActionPerformed(evt);
+                btnHActionPerformed(evt);
             }
         });
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/icons8-calendar-100.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(lblMesangem))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(171, 171, 171)
-                        .addComponent(btnClick)))
-                .addContainerGap(112, Short.MAX_VALUE))
+                    .addComponent(lblHo)
+                    .addComponent(jLabel1)
+                    .addComponent(btnH))
+                .addContainerGap(157, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addComponent(lblMesangem, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(btnClick)
-                .addGap(21, 21, 21))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addGap(15, 15, 15)
+                        .addComponent(lblHo)
+                        .addGap(26, 26, 26)
+                        .addComponent(btnH))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnClickActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClickActionPerformed
-        lblMesangem.setText("Ola, Mundo");
-    }//GEN-LAST:event_btnClickActionPerformed
+    private void btnHActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnHActionPerformed
+       Date relogio = new Date();
+       lblHo.setText(relogio.toString());
+    }//GEN-LAST:event_btnHActionPerformed
 
     /**
      * @param args the command line arguments
@@ -92,26 +103,28 @@ public class OlaMundoJava extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(OlaMundoJava.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaRelogio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(OlaMundoJava.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaRelogio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(OlaMundoJava.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaRelogio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(OlaMundoJava.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaRelogio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new OlaMundoJava().setVisible(true);
+                new TelaRelogio().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnClick;
-    private javax.swing.JLabel lblMesangem;
+    private javax.swing.JButton btnH;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lblHo;
     // End of variables declaration//GEN-END:variables
 }
